0c372dd8be2360c18c379a9b84e1b532b0a7b6cb
unknown <saksham07chauhan@gmail.com>
2024-06-02 11:17:06 -0700

    bookapp version-1

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Unleash your next adventure with just a click. Our app puts a
+            universe of stories at your fingertips. Browse millions of books,
+            discover hidden gems with personalized recommendations, and satisfy
+            your inner bookworm – anytime, anywhere. It's your personal library,
+            always open,{" "}
+            <span className="text-pink-500">
+              always waiting to fuel your imagination!!
+            </span>
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
